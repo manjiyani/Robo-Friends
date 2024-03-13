c089f22831e52f68843059df1c69d5385813e235
manjiyani <manjiyani.alim@gmail.com>
2024-03-13 16:08:52 -0400

    Replaced classes with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,57 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css';
 
-class App extends Component{
-    constructor(){
-        super();
-        this.state = {
-            roboList : [],
-            searchval : ""
-        }
+function App () {
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         roboList : [],
+    //         searchval : ""
+    //     }
+    // }
+
+    // componentDidMount() {
+    //     fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => this.setState({roboList: users}));
+    // }
+
+    const [roboList, setRoboList] = useState([]);
+    const [searchval, setSearchval] = useState("");
+
+    useEffect(()=> {
+        fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => setRoboList(users));
+    }, []);
+
+    const sschanger = (event) => {
+        setSearchval(event.target.value);     
     }
 
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => this.setState({roboList: users}));
+    const filteredRobots = roboList.filter(robos => {
+        return robos.name.toLowerCase().includes(searchval.toLowerCase());
+    })
+
+    if (roboList.length === 0){
+        return (<h1>Loading</h1>);
     }
-
-    sschanger = (event) => {
-        this.setState({searchval: event.target.value});     
+    else{
+        return(
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange= {sschanger}/>
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardList robots = {filteredRobots} />
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+        );
     }
-
-    render() {
-        const filteredRobots = this.state.roboList.filter(robos => {
-            return robos.name.toLowerCase().includes(this.state.searchval.toLowerCase());
-        })
-
-        if (this.state.roboList.length === 0){
-            return (<h1>Loading</h1>);
-        }
-        else{
-            return(
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange= {this.sschanger}/>
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots = {filteredRobots} />
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            );
-        }
 
         
-    }
+    
 }
 
 export default App
